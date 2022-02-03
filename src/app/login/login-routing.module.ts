0d3a856ee8bf346c686a69233cd6d703fd78847c
Sasu Korhonen <smage90@hotmail.com>
2022-02-03 14:45:04 +0200

    fix: fixed auth guard not redirecting already logged users

--- src/app/login/login-routing.module.ts
+++ src/app/login/login-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { LoginPage } from '../login/login.page';
 import { Routes } from '@angular/router';
 import { RouterModule } from '@angular/router';
+import { isLoggedIn } from '../services/guardService/isLoggedIn.service';
 
 const routes: Routes = [
   {
     path: '',
     component: LoginPage,
+    canActivate: [isLoggedIn]
   },
 ];
 
---
+++ src/app/services/guardService/isLoggedIn.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Router, CanActivate } from '@angular/router';
+
+@Injectable()
+export class isLoggedIn implements CanActivate {
+  constructor(private _router: Router) {}
+
+  canActivate(): boolean {
+    if (localStorage.getItem('trainer') !== null) {
+      this._router.navigate(['trainer']);
+      return false;
+    } else  
+      return true;
+  }
+}
