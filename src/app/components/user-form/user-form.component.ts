bce794a69eb72f8796451e055d7f9f52c7f33511
Julius <huttunen.julius@gmail.com>
2022-02-03 10:14:41 +0200

    feat(user login): first iteration of the user login functionality

--- src/app/components/user-form/user-form.component.ts
+++ src/app/components/user-form/user-form.component.ts
@@ -1,4 +1,7 @@
 import { Component} from "@angular/core";
+import { getUserFromAPI } from "src/app/services/trainerAPI/trainerAPI";
+
+
 
 @Component({
     selector: 'app-user-form',
@@ -6,5 +9,17 @@ import { Component} from "@angular/core";
     styleUrls: ['./user-form.component.css']
 })
 export class UserFormComponent {
+
+    userName:string = ''
+
+    public handleUser = async (userName:string) => {
+        const [error, user] = await getUserFromAPI(userName)
+        console.log("ERR", error)
+        console.log("USER", user)
+    }
+
+    public handleClick = () => {
+        this.handleUser(this.userName)
+    }
     
 }
