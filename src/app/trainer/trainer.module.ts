660b4489f87dae4052eaf63254969f82d2aee37d
Julius <huttunen.julius@gmail.com>
2022-02-04 10:17:37 +0200

    feat(trainer page): trainer's pokemons are now displayed on the trainer page

--- src/app/trainer/trainer.module.ts
+++ src/app/trainer/trainer.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { TrainerPage } from './trainer.page';
 import { TrainerRoutingModule } from './trainer-routing.module';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [TrainerPage],
-  imports: [TrainerRoutingModule],
+  imports: [TrainerRoutingModule, CommonModule],
 })
 export class TrainerModule {}
