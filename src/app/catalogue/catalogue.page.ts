91ec211f41674a850d8b738dc145a2bf589fa22e
Julius <huttunen.julius@gmail.com>
2022-02-04 12:58:27 +0200

    feat: made the catalogue and trainer page look cooler

--- src/app/catalogue/catalogue.page.ts
+++ src/app/catalogue/catalogue.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { Pokemon } from 'src/app/models/pokemon.model';
 import { PokemonsService } from 'src/app/services/pokemonAPI/pokemons.service';
 import { TrainerService } from '../services/trainerAPI/trainer.service';
@@ -10,7 +10,8 @@ import { TrainerService } from '../services/trainerAPI/trainer.service';
 })
 export class CataloguePage implements OnInit {
   constructor(private readonly pokemonService: PokemonsService,
-    private readonly trainerService: TrainerService) {}
+    private readonly trainerService: TrainerService) { }
+
   ngOnInit(): void {
     if (sessionStorage.getItem('pokemons') === null)
       this.pokemonService.fetchPokemons();
@@ -20,7 +21,11 @@ export class CataloguePage implements OnInit {
     return this.pokemonService.pokemons();
   }
 
-  handleItemClick = (pokemon: Pokemon) => {
+  handleItemClick = (pokemon: Pokemon, id: any) => {
     this.trainerService.catchPokemon(pokemon.name)
+    console.log(id)
   };
+
 }
+
+
