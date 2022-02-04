1786aed3dfa9b4b4ecec6c6ba8f624f7728dd734
Julius <huttunen.julius@gmail.com>
2022-02-04 15:09:04 +0200

    feat(catalogue): visual pointers for pokemon that already has been caught. content changes mirroring user actions.

--- src/app/catalogue/catalogue.page.ts
+++ src/app/catalogue/catalogue.page.ts
@@ -14,6 +14,8 @@ export class CataloguePage implements OnInit {
     private readonly pokemonService: PokemonsService,
     private readonly trainerService: TrainerService
   ) {}
+
+  content: string = "Click on a Pokemon to add it to your collection."
   
   ngOnInit(): void {
     if (sessionStorage.getItem('pokemons') === null)
@@ -24,15 +26,22 @@ export class CataloguePage implements OnInit {
     return this.pokemonService.pokemons();
   }
 
+  pokemonInCollection(pokemon: Pokemon) {
+    const _trainer: User = JSON.parse(localStorage.getItem('user') || '[]');
+    if (!this.trainerService.isOwned(pokemon, _trainer)){
+      return true
+    }
+    else return false
+  }
+
   handleItemClick = (pokemon: Pokemon) => {
     const _trainer: User = JSON.parse(localStorage.getItem('user') || '[]');
     const _pokemons: string[] = [..._trainer.pokemon, pokemon.name];
-
     if (this.trainerService.isOwned(pokemon, _trainer)) {
+      this.content = "You caught " + (pokemon.name[0].toUpperCase()) + (pokemon.name.slice(1)) + "!"
       this.trainerService.catchPokemon(_pokemons, _trainer);
-      alert(`${pokemon.name} CAUGHT!`)
     } else {
-      alert('Pokemon already caught')
+      this.content = "You already have " + (pokemon.name[0].toUpperCase()) + (pokemon.name.slice(1)) + "."
     }
   };
 
